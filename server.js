501edad611cddd5b5cfaf14beb2924c855300c48
Sureca <sureca@vyakya.com>
2017-02-17 14:39:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,65 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var htmlContent = {
-    'valueHtml1': {   
-        title : `One`,
-        heading : `First app`,
-        content : `This is the testing application`,
-        date : `date`
-    },
-    'valueHtml2':{
-        title : `Two`,
-        heading : `Second app`,
-        content : `This is the second testing application`,
-        date : `date`
-    }
-};
-
-function createHtml (input){
-    var title = input.title;
-    var content = input.content;
-    var date = input.date;
-    var heading = input.heading;
-    var htmlTemplate = `
-    <html>
-        <head>
-            <title>
-                ${title}
-            </title>
-            <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-            
-        <body>
-            <div class="container">
-                <div>
-                    <a href="/">Home</a>
-                </div>
-                <hr/>
-                <h3>
-                    ${heading}
-                </h3>
-                <div>
-                    ${date}
-                </div>
-                <div>
-                    <p>
-                        ${content}
-                    </p>
-                </div>
-            </div>
-        </body>
-    </html>
-    `;
-    return htmlTemplate;
-}
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/:test', function (req, res) {
-  
 });
 
 app.get('/ui/style.css', function (req, res) {
