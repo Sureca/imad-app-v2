210e8fe7673716332af092dc72bf1f87fe20a182
Sureca <sureca@vyakya.com>
2017-02-27 13:37:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,58 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+var htmlContent = {
+    'valueHtml1': {   
+        title : `One`,
+        heading : `First app`,
+        content : `This is the testing application`,
+        date : `date`
+    },
+    'valueHtml2':{
+        title : `Two`,
+        heading : `Second app`,
+        content : `This is the second testing application`,
+        date : `date`
+    }
+};
+
+function createHtml (input){
+    var title = input.title;
+    var content = input.content;
+    var date = input.date;
+    var heading = input.heading;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+            
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    <p>
+                        ${content}
+                    </p>
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -33,6 +85,11 @@ app.get('/test-db',function(req,res){
         }
         
     });
+});
+
+app.get('/:test', function (req, res) {
+  var value = req.params.test
+  res.send(createHtml(htmlContent[value]));
 });
 
 app.get('/ui/madi.png', function (req, res) {
